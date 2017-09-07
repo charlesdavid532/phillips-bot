4a53feb3439034562d08bc363927d85585e35066
David <cdavid@deloitte.com>
2017-09-08 01:18:15 +0530

    Added print statements
    -- convert datetime to date

--- app.py
+++ app.py
@@ -515,7 +515,7 @@ def getStrDefaultRegion():
     return "North East"
 
 def getStrDefaultStartDate():
-    return dt.date(2017,01,01).strptime("%Y-%m-%d")
+    return dt.datetime(2017,1,1,00,00).date().strptime("%Y-%m-%d")
 
 def getStrDefaultEndDate():
     return dt.today().strftime('%Y-%m-%d')
