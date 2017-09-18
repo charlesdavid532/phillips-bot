5ea667e8e1b8c58bc6f7c0b36e719852421ac325
David <cdavid@deloitte.com>
2017-09-18 23:26:21 +0530

    Add import statements for email

--- custom_email.py
+++ custom_email.py
@@ -1,6 +1,8 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 import os
 import boto3
 from botocore.client import Config
