29b732823962206d1aa37395535209fc20152100
David <cdavid@deloitte.com>
2017-10-11 08:30:24 +0530

    Add rauth

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ import matplotlib.pyplot as plt
 import io
 import uuid
 from custom_email import Email
+from rauth import OAuth2Service
+import urllib2
 
 try:
     import apiai
@@ -59,6 +61,13 @@ app.config['MONGO_DBNAME'] = 'phillips-sales-data'
 app.config['MONGO_URI'] = 'mongodb://admin:admin@ds123124.mlab.com:23124/phillips-sales-data'
 app.config['ASSIST_ACTIONS_ON_GOOGLE'] = True
 
+app.config['OAUTH_CREDENTIALS'] = {
+    'google': {
+            'id': os.environ['GOOGLE_LOGIN_CLIENT_ID'],
+            'secret': os.environ['GOOGLE_LOGIN_CLIENT_SECRET']
+    }
+}
+
 mongo = PyMongo(app)
 
 
@@ -67,6 +76,72 @@ class JSONEncoder(json.JSONEncoder):
         if isinstance(o, ObjectId):
             return str(o)
         return json.JSONEncoder.default(self, o)
+
+
+
+class OAuthSignIn(object):
+    providers = None
+
+    def __init__(self, provider_name):
+        self.provider_name = provider_name
+        credentials = app.config['OAUTH_CREDENTIALS'][provider_name]
+        self.consumer_id = credentials['id']
+        self.consumer_secret = credentials['secret']
+
+    def authorize(self):
+        pass
+
+    def callback(self):
+        pass
+
+    def get_callback_url(self):
+        return url_for('oauth_callback', provider=self.provider_name,
+                        _external=True)
+
+    @classmethod
+    def get_provider(self, provider_name):
+        if self.providers is None:
+            self.providers={}
+            for provider_class in self.__subclasses__():
+                provider = provider_class()
+                self.providers[provider.provider_name] = provider
+        return self.providers[provider_name]
+
+class GoogleSignIn(OAuthSignIn):
+    def __init__(self):
+        super(GoogleSignIn, self).__init__('google')
+        #TODO this is an external file. Need to host it on my server        
+        googleinfo = urllib2.urlopen('https://accounts.google.com/.well-known/openid-configuration')
+        google_params = json.load(googleinfo)
+        self.service = OAuth2Service(
+                name='google',
+                client_id=self.consumer_id,
+                client_secret=self.consumer_secret,
+                authorize_url=google_params.get('authorization_endpoint'),
+                base_url=google_params.get('userinfo_endpoint'),
+                access_token_url=google_params.get('token_endpoint')
+        )
+
+    def authorize(self):
+        return redirect(self.service.get_authorize_url(
+            scope='email',
+            response_type='code',
+            redirect_uri=self.get_callback_url())
+            )
+
+    def callback(self):
+        if 'code' not in request.args:
+            return None, None, None
+        oauth_session = self.service.get_auth_session(
+                data={'code': request.args['code'],
+                      'grant_type': 'authorization_code',
+                      'redirect_uri': self.get_callback_url()
+                     },
+                decoder = json.loads
+        )
+        me = oauth_session.get('').json()
+        return (me['name'],
+                me['email'])
 
 class User():
 
@@ -117,7 +192,7 @@ def index():
 '''
 
 @app.route('/', methods=['GET'])
-def verify():
+def index():
     print ("Hellow world")
 	# Webhook verification
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
@@ -134,6 +209,53 @@ def verifyTemp():
     return "Hello world this is the login protected page", 200
 
 
+@app.route('/authorize/<provider>')
+def oauth_authorize(provider):
+    # Flask-Login function
+    if not current_user.is_anonymous():
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    return oauth.authorize()
+
+@app.route('/callback/<provider>')
+def oauth_callback(provider):
+    if not current_user.is_anonymous():
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    username, email = oauth.callback()
+    if email is None:
+        # I need a valid email address for my user identification
+        console.log('Authentication failed')
+        #flash('Authentication failed.')
+        return redirect(url_for('index'))
+    # Look if the user already exists
+    users = mongo.db.users
+    loginUser = users.find_one({'username' : form.username.data})
+    user_obj = User(loginUser['username'])
+    #user=User.query.filter_by(email=email).first()
+    if not loginUser:
+        # Create the user. Try and use their name returned by Google,
+        # but if it is not set, split the email address at the @.
+        '''
+        nickname = username
+        if nickname is None or nickname == "":
+            nickname = email.split('@')[0]
+        '''
+        # We can do more work here to ensure a unique nickname, if you 
+        # require that.
+        '''
+        user=User(nickname=nickname, email=email)
+        db.session.add(user)
+        db.session.commit()
+        '''
+        user_obj = User(email)
+    # Log in the user, by default remembering them for their next visit
+    # unless they log out.
+    '''
+    login_user(user, remember=True)
+    '''
+    login_user(user_obj)
+    return redirect(url_for('index'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
