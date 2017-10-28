c30130a782b5a471bdbec48f583aac4cc4bb910e
David <cdavid@deloitte.com>
2017-10-28 13:02:49 +0530

    Remove space and tab mixing

--- amazon_s3.py
+++ amazon_s3.py
@@ -10,12 +10,11 @@ class AmazonS3(object):
 		self.s3 = boto3.resource('s3',aws_access_key_id=os.environ['S3_KEY'],aws_secret_access_key=os.environ['S3_SECRET'],config=Config(signature_version='s3v4'))
 		self.bucketname = bucketname
 
-		
 	def saveResourceToAWS(self, img_data, img_name, content_type, bucketname):
 		print("saving resource to aws in amazon s3")
-	    #self.s3.Bucket(BUCKET_NAME).put_object(Key=img_name, Body=img_data, ContentType=content_type)
-	    self.s3.Bucket(bucketname).put_object(Key=img_name, Body=img_data, ContentType=content_type)
-	    print("Saved resource to aws")
+		#self.s3.Bucket(BUCKET_NAME).put_object(Key=img_name, Body=img_data, ContentType=content_type)
+		self.s3.Bucket(bucketname).put_object(Key=img_name, Body=img_data, ContentType=content_type)
+		print("Saved resource to aws")
 		
 
 	def getS3Object(self, bucketname, filename):
