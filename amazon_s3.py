5ac40fe7c64f4da70c0887f73897d66bf161f4aa
David <cdavid@deloitte.com>
2017-10-28 12:58:44 +0530

    Change indentation of amazon s3

--- amazon_s3.py
+++ amazon_s3.py
@@ -7,19 +7,12 @@ class AmazonS3(object):
 	def __init__(self, bucketname):
 		super(AmazonS3, self).__init__()
 
-		self.s3 = boto3.resource('s3',
-			        aws_access_key_id=os.environ['S3_KEY'],
-			        aws_secret_access_key=os.environ['S3_SECRET'],
-			        config=Config(signature_version='s3v4')
-			        )
-
+		self.s3 = boto3.resource('s3',aws_access_key_id=os.environ['S3_KEY'],aws_secret_access_key=os.environ['S3_SECRET'],config=Config(signature_version='s3v4'))
 		self.bucketname = bucketname
 
-
-
-
-    def saveResourceToAWS(self, img_data, img_name, content_type, bucketname):
-	    print("saving resource to aws in amazon s3")
+		
+	def saveResourceToAWS(self, img_data, img_name, content_type, bucketname):
+		print("saving resource to aws in amazon s3")
 	    #self.s3.Bucket(BUCKET_NAME).put_object(Key=img_name, Body=img_data, ContentType=content_type)
 	    self.s3.Bucket(bucketname).put_object(Key=img_name, Body=img_data, ContentType=content_type)
 	    print("Saved resource to aws")
