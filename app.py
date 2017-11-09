fcbd914362bd3bff362eed9a8261645f26c24c0a
David <cdavid@deloitte.com>
2017-11-09 13:13:33 +0530

    Added get method for token callback

--- app.py
+++ app.py
@@ -497,7 +497,7 @@ def oauth_authorize(provider):
     oauth = OAuthSignIn.get_provider(provider)
     return oauth.authorize()
 
-@app.route('/callback/<provider>')
+@app.route('/callback/<provider>', methods=['GET'])
 def oauth_callback(provider):
     '''
     if not current_user.is_anonymous():
