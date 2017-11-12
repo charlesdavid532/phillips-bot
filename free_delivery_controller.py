9fa8506745ada30bcd17fb87c3a02a5a0ca81eeb
David <cdavid@deloitte.com>
2017-11-12 20:52:33 +0530

    Add the list index

--- free_delivery_controller.py
+++ free_delivery_controller.py
@@ -124,6 +124,6 @@ class FreeDeliveryController(object):
 		locationParserObj.setBaseLocation(latitude, longitude)
 		locationParserObj.setObjectLocations(storeLoc)
 		nearestStore = locationParserObj.getNNearestLocations(1)
-		print("The nearest store distance in kms is:::" + str(nearestStore["distance"]))
-		return "Yes you have free delivery since you are only " + str(nearestStore["distance"]) + " km away"
+		print("The nearest store distance in kms is:::" + str(nearestStore[0]["distance"]))
+		return "Yes you have free delivery since you are only " + str(nearestStore[0]["distance"]) + " km away"
 		
