cef52130e077b8e7843bf607920614bd56d44858
David <cdavid@deloitte.com>
2017-11-21 10:43:55 +0530

    Added google assistant header v2

--- app.py
+++ app.py
@@ -887,6 +887,7 @@ def handle_message():
     print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
+    r.headers['Google-Assistant-API-Version'] = 'v2'
     print("Before final return")
     return r
 
