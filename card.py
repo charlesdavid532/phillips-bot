6561b801292f2972031f367868b17ad9243423b0
David <cdavid@deloitte.com>
2017-11-26 12:01:14 +0530

    Fix indent issue in Google Card

--- card.py
+++ card.py
@@ -60,13 +60,13 @@ class Card(object):
 		pass
 
 	@classmethod
-    def get_provider(self, provider_name):
-        if self.providers is None:
-            self.providers={}
-            for provider_class in self.__subclasses__():
-                provider = provider_class()
-                self.providers[provider.provider_name] = provider
-        return self.providers[provider_name]
+	def get_provider(self, provider_name):
+		if self.providers is None:
+			self.providers={}
+			for provider_class in self.__subclasses__():
+				provider = provider_class()
+				self.providers[provider.provider_name] = provider
+		return self.providers[provider_name]
 
 
 class GoogleCard(Card):
